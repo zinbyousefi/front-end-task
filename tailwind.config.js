ad0f4331dd1b51a29737b0a6d8e98e74acf45b30
zinbyousefi <yousefizeinb@gmail.com>
2025-02-14 17:53:03 +0330

    fix: :zap: remove unused css in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#0ab2b3",
         secondary: "#8dbf2a",
-      },
-      lineClamp: {
-        2: "2",
       },
     },
   },
